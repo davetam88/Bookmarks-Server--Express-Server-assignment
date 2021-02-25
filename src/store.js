e75fe3f439fe3646e2374366f52d1174d84e57c0
David Tam <davetam88@gmail.com>
2021-02-25 18:12:09 -0500

    data format fix

--- src/store.js
+++ src/store.js
@@ -1,83 +1,40 @@
-<<<<<<< HEAD
 const bookmarks = [
-{
-    id: 1,
-    title: 'title 1', 
-    url: 'url 1', 
-    description: 'description 1', 
-    rating: 'rating 1', 
-},
-{
-    id: 2,
-    title: 'title 2', 
-    url: 'url 2', 
-    description: 'description 2', 
-    rating: 'rating 2', 
-},
-{
-    id: 3,
-    title: 'title 3', 
-    url: 'url 3', 
-    description: 'description 3', 
-    rating: 'rating 3', 
-},
-{
-    id: 4,
-    title: 'title 4', 
-    url: 'url 4', 
-    description: 'description 4', 
-    rating: 'rating 4', 
-},
-{
-    id: 5,
-    title: 'title 5', 
-    url: 'url 5', 
-    description: 'description 5', 
-    rating: 'rating 5', 
-},
+    {
+        id: 1,
+        title: 'title 1',
+        url: 'url 1',
+        description: 'description 1',
+        rating: 'rating 1',
+    },
+    {
+        id: 2,
+        title: 'title 2',
+        url: 'url 2',
+        description: 'description 2',
+        rating: 'rating 2',
+    },
+    {
+        id: 3,
+        title: 'title 3',
+        url: 'url 3',
+        description: 'description 3',
+        rating: 'rating 3',
+    },
+    {
+        id: 4,
+        title: 'title 4',
+        url: 'url 4',
+        description: 'description 4',
+        rating: 'rating 4',
+    },
+    {
+        id: 5,
+        title: 'title 5',
+        url: 'url 5',
+        description: 'description 5',
+        rating: 'rating 5',
+    },
 ];
 
 module.exports = { bookmarks }
 
-=======
-const bookmarks = [
-{
-    id: 1,
-    title: 'title 1', 
-    url: 'url 1', 
-    description: 'description 1', 
-    rating: 'rating 1', 
-},
-{
-    id: 2,
-    title: 'title 2', 
-    url: 'url 2', 
-    description: 'description 2', 
-    rating: 'rating 2', 
-},
-{
-    id: 3,
-    title: 'title 3', 
-    url: 'url 3', 
-    description: 'description 3', 
-    rating: 'rating 3', 
-},
-{
-    id: 4,
-    title: 'title 4', 
-    url: 'url 4', 
-    description: 'description 4', 
-    rating: 'rating 4', 
-},
-{
-    id: 5,
-    title: 'title 5', 
-    url: 'url 5', 
-    description: 'description 5', 
-    rating: 'rating 5', 
-},
-];
-
-module.exports = { bookmarks }
-
->>>>>>> 52e1926dfb6379cfca6b1cdd251c94a7ffa536b8
